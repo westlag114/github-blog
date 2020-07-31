1dde5ccb394f751fb918e18c06696ecdcaaa17d9
westlag114 <fsww2353@gmail.com>
2020-07-31 18:23:54 +0900

    apollo introduction

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Apollo App</h2>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
