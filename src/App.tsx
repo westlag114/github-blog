ce577623e44f67406278d16cdfdd80be485ce72f
westlag114 <fsww2353@gmail.com>
2020-08-01 20:09:26 +0900

    appにclient移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: { authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}` },
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        viewer {
+          login
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Apollo App</h2>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,40 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloProvider,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  headers: { authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}` },
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-      query {
-        viewer {
-          login
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        <h2>Apollo App</h2>
-      </div>
-    </ApolloProvider>
-  );
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
